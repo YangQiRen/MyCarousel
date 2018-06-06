4f0d356b4fb684d698cefedc5bebe57ab280b374
Hulk <hulkyang@taipay.com>
2018-06-06 16:24:23 +0800

    [First Commit] 基本的都創建完成，將TestAdapter修改看要塞什麼物件

---
+++ app/src/main/java/com/yang/mycarousel/MainActivity.java
@@ -0,0 +1,125 @@
+package com.yang.mycarousel;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.azoft.carousellayoutmanager.CarouselLayoutManager;
+import com.azoft.carousellayoutmanager.CarouselZoomPostLayoutListener;
+import com.azoft.carousellayoutmanager.CenterScrollListener;
+import com.azoft.carousellayoutmanager.DefaultChildSelectionListener;
+
+import java.util.Locale;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    public TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.hello_world);
+
+        final TestAdapter adapter = new TestAdapter();
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        // create layout manager with needed params: vertical, cycle
+        initRecyclerView(recyclerView, new CarouselLayoutManager(CarouselLayoutManager.HORIZONTAL, false), adapter);
+
+
+    }
+
+    private void initRecyclerView(final RecyclerView recyclerView, final CarouselLayoutManager layoutManager, final TestAdapter adapter) {
+        // enable zoom effect. this line can be customized
+        layoutManager.setPostLayoutListener(new CarouselZoomPostLayoutListener());
+        layoutManager.setMaxVisibleItems(2);
+
+        recyclerView.setLayoutManager(layoutManager);
+        // we expect only fixed sized item for now
+        recyclerView.setHasFixedSize(true);
+        // sample adapter with random data
+        recyclerView.setAdapter(adapter);
+        // enable center post scrolling
+        recyclerView.addOnScrollListener(new CenterScrollListener());
+        // enable center post touching on item and item click listener
+        DefaultChildSelectionListener.initCenterItemListener(new DefaultChildSelectionListener.OnCenterItemClickListener() {
+            @Override
+            public void onCenterItemClicked(@NonNull final RecyclerView recyclerView, @NonNull final CarouselLayoutManager carouselLayoutManager, @NonNull final View v) {
+                final int position = recyclerView.getChildLayoutPosition(v);
+                final String msg = String.format(Locale.US, "Item %1$d was clicked", position);
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+        }, recyclerView, layoutManager);
+
+        layoutManager.addOnItemSelectionListener(new CarouselLayoutManager.OnCenterItemSelectionListener() {
+
+            @Override
+            public void onCenterItemChanged(final int adapterPosition) {
+                if (CarouselLayoutManager.INVALID_POSITION != adapterPosition) {
+                    final int value = adapter.mPosition[adapterPosition];
+                    textView.setText("position:" + value);
+/*
+                    adapter.mPosition[adapterPosition] = (value % 10) + (value / 10 + 1) * 10;
+                    adapter.notifyItemChanged(adapterPosition);
+*/
+                }
+            }
+        });
+    }
+
+    private static final class TestAdapter extends RecyclerView.Adapter<TestViewHolder> {
+
+        @SuppressWarnings("UnsecureRandomNumberGeneration")
+        private final Random mRandom = new Random();
+        private final int[] mColors;
+        private final int[] mPosition;
+        private int mItemsCount = 10;
+
+        TestAdapter() {
+            mColors = new int[mItemsCount];
+            mPosition = new int[mItemsCount];
+            for (int i = 0; mItemsCount > i; ++i) {
+                //noinspection MagicNumber
+                mColors[i] = Color.argb(255, mRandom.nextInt(256), mRandom.nextInt(256), mRandom.nextInt(256));
+                mPosition[i] = i;
+            }
+        }
+
+        @Override
+        public TestViewHolder onCreateViewHolder(@NonNull final ViewGroup parent, final int viewType) {
+
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_view, parent, false);
+            return new TestViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(final TestViewHolder holder, final int position) {
+            holder.itemView.setBackgroundColor(mColors[position]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mItemsCount;
+        }
+    }
+
+    private static class TestViewHolder extends RecyclerView.ViewHolder {
+
+
+        TestViewHolder(final View view) {
+            super(view);
+
+        }
+    }
+}
